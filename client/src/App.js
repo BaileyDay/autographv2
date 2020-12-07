1d0200ea2b7d2c19bd558da68ec8699d40ef32d2
BaileyDay <bailey.j.day@gmail.com>
2020-12-06 22:01:14 -0500

    Finished navbar for v2 of autograph

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navbar from './components/navbar'
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
