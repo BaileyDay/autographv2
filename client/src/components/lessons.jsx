aefd4900932983401cee80b6bfb67893ba9fc013
BaileyDay <bailey.j.day@gmail.com>
2020-12-10 16:58:00 -0500

    finished lessons

--- client/src/components/lessons.jsx
+++ client/src/components/lessons.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from './navbar'
+import Footer from './footer'
 import '../css/lessons.css'
 
 const Lessons = () => {
@@ -16,14 +17,29 @@ const Lessons = () => {
   </div>
 </div>
 </section>
+<div className="container">
       <div className="columns">
-          <div className="column">
-              <h1>Hello</h1>
+          <div className="lessonsLeft column is-family-sans-serif">
+              <p>At Autograph Farm we offer lessons for all levels of riders ages four and up. Whether you want to lesson just for fun or show at some of the top shows in the country, we can happily accommodate you!<br></br><br></br>
+
+If you do not own a horse, no problem - we have eight sweet horses and ponies available for lessons at varying skill sets. Several of these are even leasable for shows. We offer group lessons for all ages, as well as private lessons for those preferring individual instruction.</p>
           </div>
-          <div className="column">
-              <h1>Test</h1>
+          <div className="column lessonsIMG">
+              <img src="https://autographfarm.s3.us-east-2.amazonaws.com/lessons01.jpeg" alt=""/>
           </div>
       </div>
+      </div>
+      <div className="container">
+      <div className="columns">
+          <div className="lessonsLeft column is-family-sans-serif">
+              <p>We also emphasize horse care as an integral part of horsemanship. As such, we spend time on the fundamentals of horse care before and after lessons. We also provide opportunities outside of lessons for those wishing to further advance their horse-care skills.</p>
+          </div>
+          <div className="column lessonsIMG">
+              <img src="https://autographfarm.s3.us-east-2.amazonaws.com/lessons03.jpeg" alt=""/>
+          </div>
+      </div>
+      </div>
+      <Footer/>
 </>
   );
 };
