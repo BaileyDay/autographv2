963206e4778650a0a00646039d687b5cdb8b459a
BaileyDay <bailey.j.day@gmail.com>
2020-12-10 21:28:50 -0500

    added .env files

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config()
 const cors = require('cors');
 const path = require('path')
 const app = express();
 
 app.use(bodyParser.json());
 
-
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
-});
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res) => {
