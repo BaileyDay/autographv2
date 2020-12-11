40668b117a18e0db6c9de7f6b3e8aa01e13a87ad
BaileyDay <bailey.j.day@gmail.com>
2020-12-10 21:37:36 -0500

    added user model

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ require('dotenv').config()
 const cors = require('cors');
 const path = require('path')
 const app = express();
+const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGO
 
 app.use(bodyParser.json());
-
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).catch((err) => {
+  console.log(err)
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
