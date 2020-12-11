58a49944a6fab71389805e0cc54e245b686b28ed
BaileyDay <bailey.j.day@gmail.com>
2020-12-10 20:16:48 -0500

    added boarding page

--- client/src/components/boarding.jsx
+++ client/src/components/boarding.jsx
@@ -32,15 +32,38 @@ const Boarding = () => {
           <div className="column lessonsIMG">
               <img src="https://autographfarm.s3.us-east-2.amazonaws.com/Boarding/BoardingTwo.3ee6df3d.jpg" alt=""/>
           </div>
+          
       </div>
       </div>
-      <div className="container">
+      <div className="container ">
+      <p className="has-text-centered m-5">Our Barn is very reasonably sized so that we are able to care for each horses specific needs.</p>
       <div className="columns">
           <div className="lessonsLeft column is-family-sans-serif">
-              <p>We also emphasize horse care as an integral part of horsemanship. As such, we spend time on the fundamentals of horse care before and after lessons. We also provide opportunities outside of lessons for those wishing to further advance their horse-care skills.</p>
+              <h2 className="title">Open Pastures</h2>
+              <p>We have spacious grassy pastures that our horses are able to enjoy all day or night, depending on the season. We offer individual or small group turnout, and we supplement with hay throughout the winter.</p>
           </div>
           <div className="column lessonsIMG">
-              <img src="https://autographfarm.s3.us-east-2.amazonaws.com/lessons03.jpeg" alt=""/>
+              <img src="https://autographfarm.s3.us-east-2.amazonaws.com/Boarding/pastures.ab0df118.jpg" alt=""/>
+          </div>
+      </div>
+      <div className="columns">
+      <div className="column lessonsIMG">
+              <img src="https://autographfarm.s3.us-east-2.amazonaws.com/Boarding/Stalls.f4840443.jpg" alt=""/>
+          </div>
+          <div className="lessonsLeft column is-family-sans-serif">
+              <h2 className="title">Spacious Stalls</h2>
+              <p>We have large 12' x 14' stalls that are cleaned daily and heavily bedded at all times.</p>
+          </div>
+      </div>
+      <div className="columns">
+          <div className="lessonsLeft column is-family-sans-serif">
+              <h2 className="title">Happy Horses</h2>
+              <p>We offer four different types of grain, three different types of hay, and are happy to feed supplements to make sure every horses needs are met. We also have hay nets for every horse, to insure that they always have something to eat and occupy their time indoors.<br></br><br></br>
+
+The barn owner lives on property and the barn manager lives less than 5 minutes away. The entire property is fenced in and gated to insure your horses safety.</p>
+          </div>
+          <div className="column lessonsIMG">
+              <img src="https://autographfarm.s3.us-east-2.amazonaws.com/Boarding/pastures.ab0df118.jpg" alt=""/>
           </div>
       </div>
       </div>
